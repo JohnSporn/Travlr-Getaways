cd4a2fe0a63c04345b6431fc557a3b385f00e125
jsporn1 <jms96@optonline.net>
2022-05-24 20:45:50 -0400

    Converting travel and rooms template from static to dynamic JSON

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+const fs = require('fs'); // filesystem
+const roomTypes = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 // GET Homepage
 const rooms = (req, res) => {
-    res.render('rooms', {title: 'Travlr Getaways'});
+    res.render('rooms', {title: 'Travlr Getaways', roomTypes});
 };
 
 module.exports = {
--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+
+const fs = require('fs'); // filesystem
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // GET Homepage
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
