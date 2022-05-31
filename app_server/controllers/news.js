4bd486b925a668a59b625e977d63b32c1803c0f8
jsporn1 <jms96@optonline.net>
2022-05-31 19:41:08 -0400

    Converting static data to dynamic JSON part 2

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,6 +1,11 @@
+const fs = require('fs'); // filesystem
+const newsTypes = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+const newsLatest = JSON.parse(fs.readFileSync('./data/latest_news.json', 'utf8'));
+const newsVacation = JSON.parse(fs.readFileSync('./data/vacation_tips.json', 'utf8'));
+
 // GET Homepage
 const news = (req, res) => {
-    res.render('news', {title: 'Travlr Getaways'});
+    res.render('news', {title: 'Travlr Getaways', newsTypes, newsLatest, newsVacation});
 };
 
 module.exports = {
