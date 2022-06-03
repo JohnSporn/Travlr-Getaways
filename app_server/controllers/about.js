a9a38dbd03349f8c1b448a30a5f8c173b655ba0e
jsporn1 <jms96@optonline.net>
2022-06-02 20:08:56 -0400

    Converting static data to dynamic json complete

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const aboutTypes = JSON.parse(fs.readFileSync('./data/about_ads.json'));
+
 // GET About Page
 const about = (req, res) => {
-    res.render('about', {title: 'Travlr Getaways'});
+    res.render('about', {title: 'Travlr Getaways', aboutTypes});
 };
 
 module.exports = {
--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const indexTypes = JSON.parse(fs.readFileSync('./data/index_blogs.json'));
+
 // GET Homepage
 const index = (req, res) => {
-    res.render('index', {title: 'Travlr Getaways'});
+    res.render('index', {title: 'Travlr Getaways', indexTypes});
 };
 
 module.exports = {
