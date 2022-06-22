8a7af849b3371a2e9b8d6aa1a69f1a739aeff77f
jsporn1 <jms96@optonline.net>
2022-06-21 21:18:45 -0400

    Angular front-end

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,36 @@
+export const trips = 
+
+[
+    {
+        "code": "GALR210214",
+        "name": "Gale Reef",
+        "length": "4 nights / 5 days",
+        "start": "2022-07-14T08:00:00Z",
+        "resort": "Emerald Bay, 3 stars",
+        "perPerson": "799.00",
+        "image": "reef1.jpg",
+        "description": "<p>At Gale....Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis. </p> <p> Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet. </p>"
+    },
+
+    {
+        "code": "DAWR210315",
+        "name": "Dawson's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2022-08-15T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1199.00",
+        "image": "reef2.jpg",
+        "description": "<p>At Dawson's....Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis. </p> <p> Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet. </p>"
+    },
+
+    {
+        "code": "CLAR210621",
+        "name": "Claire's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2022-09-21T08:00:00Z",
+        "resort": "Coral Sands, 5 stars",
+        "perPerson": "1999.00",
+        "image": "reef3.jpg",
+        "description": "<p>At Claire's....Sed et augue lorem. In sit amet placerat arcu. Mauris volutpat ipsum ac justo mollis vel vestibulum orci gravida. Vestibulum sit amet porttitor odio. Nulla facilisi. Fusce at pretium felis. </p> <p> Sed consequat libero ut turpis venenatis ut aliquam risus semper. Etiam convallis mi vel risus pretium sodales. Etiam nunc lorem ullamcorper vitae laoreet. </p>"
+    }
+]
